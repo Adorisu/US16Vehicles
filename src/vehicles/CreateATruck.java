97d40f9ba28ac78c0215710c6f02b9deabe0b121
jonesb <jonesb@PCMS-SSLT-BJ.pcmsdatafit.ad>
2016-06-12 22:15:20 +0100

    Simple nested if test

--- src/vehicles/CreateATruck.java
+++ src/vehicles/CreateATruck.java
@@ -24,11 +24,31 @@ public class CreateATruck {
         
         Object selectedCab = JOptionPane.showInputDialog(null, "Choose a Cab Type", "Cab Selection", JOptionPane.QUESTION_MESSAGE, null, cab, SUPERCAB);
         
-        JOptionPane.showMessageDialog(null, "Selected Cab " + selectedCab);
+        String[] bedLength = new String[3];
+        bedLength[0] = SHORT__BED;
+        bedLength[1] = MEDIUM__BED;
+        bedLength[2] = LONG__BED;
+        
+        final Object selectedBedLength = JOptionPane.showInputDialog(null, "Choose a Bed Length", "Bed Selection", JOptionPane.QUESTION_MESSAGE, null, bedLength, MEDIUM__BED);
+        
+        if (selectedCab.equals(SUPERCREW)) {
+            if (selectedBedLength.equals(LONG__BED)) {
+                JOptionPane.showMessageDialog(null, "Sorry, but you cannot choose both Double Cab and Long Bed.");
+            } else {
+                JOptionPane.showMessageDialog(null, "Your selections are valid.  Cab: " + selectedCab + " Bed: " + selectedBedLength);
+            }
+        }
         
     }
     
+    
+    private static final String LONG__BED = "Long Bed";
+    private static final String MEDIUM__BED = "Medium Bed";
+    private static final String SHORT__BED = "Short Bed";
+    
     private static final String REGULAR = "Regular";
     private static final String SUPERCAB = "Supercab";
-    private static final String SUPERCREW = "Supercrew";
+    private static final String SUPERCREW = "Double Cab";
+    
+    
 }
