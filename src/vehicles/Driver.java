4e4fa446f46fc52db47fee1b64aa881133c8ca15
jonesbr <brandan.jones@uc.edu>
2016-06-19 15:27:47 -0400

    Validate data with a while loop.  Video available here:  https://www.youtube.com/playlist?list=PL73qvSDlAVVj4Q_2h9Cb4Fpr6hY5GAveS

--- src/vehicles/Driver.java
+++ src/vehicles/Driver.java
@@ -5,6 +5,7 @@
  */
 package vehicles;
 
+import java.awt.HeadlessException;
 import javax.swing.JOptionPane;
 
 /**
@@ -24,7 +25,7 @@ public class Driver {
     }
     
     public static void promptUser() {
-        System.out.println("In prompt user");
+        
         
         // create an object of type Vehicle, and store it in the variable myVehicle
         Vehicle myVehicle = new Vehicle();
@@ -52,14 +53,11 @@ public class Driver {
         // set the attributes in myVehicle object.
         myVehicle.setGallonsOfGas(dblGallonsOfGas);
         
-        final String strMilesPerGallon = JOptionPane.showInputDialog("Enter miles per gallon");
-        
-        final int intMilesPerGallon = Integer.parseInt(strMilesPerGallon);
+        final int intMilesPerGallon = promptForInteger("Enter miles per gallon", 0, 100);
         
         myVehicle.setMilesPerGallon(intMilesPerGallon);
         
-        final String strOdometer = JOptionPane.showInputDialog("Enter odometer");
-        final int intOdometer = Integer.parseInt(strOdometer);
+        int intOdometer = promptForInteger("Enter odometer", 0, 1000000);
         
         myVehicle.setOdometer(intOdometer);
         
@@ -80,6 +78,32 @@ public class Driver {
         System.out.println(yourVehicle.toString());
 
         
+    }
+
+    /**
+     * Prompt and return an integer; validate that it is within a given range.
+     * 
+     * @param message The message to show when prompting the user
+     * @param low The lowest possible acceptable value for this integer.
+     * @param high The highest possible acceptable value for this integer.
+     * @return the validated integer
+     * @throws NumberFormatException when non-numeric data are entered.
+     * @throws HeadlessException 
+     */
+    public static int promptForInteger(String message, int low, int high) throws NumberFormatException, HeadlessException {
+        // prompt for input
+        String strInput = JOptionPane.showInputDialog(message);
+        
+        int intInput = Integer.parseInt(strInput);
+
+        while (!(intInput >= low && intInput <= high)) {
+            String errorMessage = "The number you have entered is not in the valid range.  Low: " + low + " High: " + high + ". Please try again.";
+            strInput = JOptionPane.showInputDialog(errorMessage);
+            intInput = Integer.parseInt(strInput);
+        }
+            
+        // return the validated number.
+        return intInput;
     }
     
 }
